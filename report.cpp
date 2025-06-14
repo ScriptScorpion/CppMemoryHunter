e2b1cd5cd1a6a4dd2c7920a99e012557eaf6c1ad
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-06-14 10:09:55 +0300

    Update report.cpp

--- report.cpp
+++ report.cpp
@@ -5,6 +5,6 @@
 void issue(std::string x,  int y) {
      std::cout << "\033[1;33m[!]\033[0m Memory leak in file named: " << x << " " << "In line number: " << y << std::endl;
 }
-// Idea 1. Add suggestion how to fix issue: Done
-// Idea 2. Add C language support
+// Idea 1. Add C language support: Done
+// Idea 2. Add suggestion how to fix issue
 #endif
